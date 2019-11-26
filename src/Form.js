ea197ad7654e3e20daab69c18c688a26fcf7b014
Sawaki <you@example.com>
2019-11-26 16:03:27 +0900

    コンポーネント作成

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <form action="">
+            <input type="text" name="desc" id=""/>
+            <button>追加</button>
+        </form>
+    );
+};
+
+export default Form;
+
+
+
---
+++ src/Item.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Item = () => {
+    return (
+        <li>
+            <p>仕事をする</p>
+            <button>完了</button>
+        </li>
+    );
+};
+
+export default Item;
+
