480899303d2d18155d81e1a19cfe32c781878acc
Sawaki <you@example.com>
2019-11-26 16:48:02 +0900

    完了・戻す切替機能

--- src/Item.js
+++ src/Item.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Item = (props) => {
+    const [isDone, setIsDone] = React.useState(false)
+    const text = isDone ? "戻す" : "完了" 
+    const handleClick = () => {
+        setIsDone(!isDone)
+    }
     return (
         <li>
             <p>{props.todo}</p>
-            <button>完了</button>
+            <button onClick={handleClick}>{text}</button>
         </li>
     );
 };
